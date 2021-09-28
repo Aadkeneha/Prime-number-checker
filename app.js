e3c09d0c3eb59b28eaeff74c4ad1c3ce2af4d9e1
Neha Aadke <aadkeneha@gmail.com>
2021-09-28 12:00:55 +0530

    feat: updated code

--- app.js
+++ app.js
@@ -2,30 +2,41 @@ var number = document.querySelector("#num");
 var result = document.querySelector("#output");
 var btnclick = document.querySelector("#check");
 
+var count = 0;
 
-function checkprimenumber(cnum){
+btnclick.addEventListener("click", function clickHandler() {
+    let status = checkPrimeNumber(number.value);
+    result.innerText = outputMessage(status);
+});
 
-    pnum = Number(cnum);
-    m = pnum/2;
-    count=0
-    
-    if (pnum===0 || pnum===1){
-        result.innerText="The given number is neither a prime number nor a composite number."
-    }
-    else
-    {
-        for(var i=2;i<=m;i++){      
-            if(pnum%i==0){      
-             result.innerText="The given number is not a prime number."   
-             var count=1;      
-             break;      
-            }  
+function checkPrimeNumber(cnum) {
+
+    pnum = String(cnum);
+    count = 0
+
+    if (pnum === '0' || pnum === '1') {
+        count = 2;
+    } else if (pnum > 0) {
+        for (var i = 2; i < pnum; i++) {
+            if (pnum % i == 0) {
+                count = 1;
+                break;
+            }
         }
+    } else if (pnum === "" || pnum < 0) {
+        count = 3;
     }
-
-    if (count==0 && number!=0){
-        result.innerText="The given number is a prime number."
-    }
+    return count;
 }
 
-btnclick.addEventListener("click",checkprimenumber(number))
+function outputMessage(count) {
+    if (count === 0) {
+        return "The given number is a prime number.";
+    } else if (count === 1) {
+        return "The given number is not a prime number.";
+    } else if (count === 2) {
+        return "The given number is neither prime nor composite";
+    } else if (count === 3) {
+        return "Enter a valid number!";
+    }
+}
